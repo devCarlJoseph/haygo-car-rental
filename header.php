a13643a1c9d9766ad98f0b5d9c3e84bcaf081b83
devCarlJoseph <carljosephsumagang17@gmail.com>
2025-10-07 22:54:29 +0800

    new changes today

---
+++ header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HayGo Car Rental</title>
+    <link rel="stylesheet" href="src/assets/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
+        integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="src/assets/css/style.css">
+
+    <script src="src/assets/js/bootstrap.js"></script>
+    <script src="src/assets/js/j_query.js"></script>
+</head>
+
+<body class="inder-regular">
+    <div class="container-fluid p-0 m-0 d-flex justify-content-center align-items-center" style="z-index: 1;">
+        <header class="bg-transparent d-flex justify-content-center align-items-center" style="margin-top: 0.5rem; ">
+            <div class="d-flex justify-content-between align-items-center" style="width: 85.5rem; height: 4.375rem;">
+                <div class="d-flex justify-content-center align-items-center" style="width: 30.313rem; height: 2.5rem;">
+                    <ul class="d-flex justify-content-between align-items-center gap-5 list-unstyled text-white"
+                        style="font-size: 1.25rem;">
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Our Fleet</li>
+                        <li>Contacts</li>
+                    </ul>
+                </div>
+                <div>
+                    <img src="src/assets/images/logo.png"
+                        style="width: 3.813rem; height: 2.438rem; margin-right: 20rem;">
+                </div>
+                <div class="d-flex justify-content-center align-items-center gap-2">
+                    <div class="bg-transparent d-flex justify-content-center align-items-center gap-1 rounded-5 border border-white text-white"
+                        style="width: 4.95rem; height: 1.75rem; font-size: 0.938rem">
+                        Help
+                        <div class="bg-transparent rounded-5 d-flex justify-content-center align-items-center border"
+                            style="width: 1.125rem; height: 1.125rem">
+                            <i class="fa-solid fa-info"></i>
+                        </div>
+                    </div>
+                    <div class="bg-light d-flex justify-content-center align-items-center"
+                        style="width: 2.188rem; height: 2.188rem; border-radius: 50%;">
+                        <i class="fa-regular fa-user" style="width: 1.25rem; height: 1.rem;"></i>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </div>
---
+++ index.php
@@ -0,0 +1,110 @@
+<?php 
+    require_once 'header.php';
+?>
+
+<section class="text-white d-flex align-items-center justify-content-center"
+            style="background-image: url('src/assets/images/hero.jpg'); background-size: contain; background-repeat: no-repeat; background-position: center; height: 70.125rem; margin-top: -13rem;">
+            <div>
+                <div style="margin-top: 3.5rem;">
+                    <div class="d-flex justify-content-between align-items-center" style="gap: 22.5rem;">
+                        <div>
+                            <p class="" style="font-size: 4.688rem;">The Best Platform <br> for Car Rental</p>
+                            <p style="font-size: 1.25rem; margin-top: -2rem;">“Hay Go - Your Journey, Your Rules”</p>
+                            <div>
+                                <button class="rounded-5"
+                                    style="width: 4.938rem; height: 1.688rem; font-size: 0.7rem; color: #474040; background-color: #D9BBAC; border: none;">Rent
+                                    Now</button>
+                            </div>
+                        </div>
+                        <div class="bg-light rounded-5" style="width: 18.563rem; height: 12.688rem;">
+
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="d-flex justify-content-end align-items-center">
+                        <p style="width: 18.375rem; font-size: 0.813rem; margin-top: 1rem;">Together, we navigate the
+                            roads of life, forging stories under the sun and stars, one ride at
+                            a time. Each journey becomes more than just distance traveled—it becomes a memory shared.
+                            Along the way, we find connections, laughter, and moments that stay with us forever. In
+                            every ride, we discover not just new places, but new stories waiting to unfold.</p>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center align-items-center">
+                    <div class="bg-light d-flex justify-content-center align-items-center"
+                        style="width: 77.75rem; height: 6.875rem; margin-top: 1.5rem; border-radius: 1.875rem; gap: 2.9rem;">
+                        <div class="bg-danger" style="width: 14.375rem; height: 4rem;">
+
+                        </div>
+                        <div style="width: 1px; height: 4rem; background: #B9B4B4;"></div>
+                        <div class="bg-danger" style="width: 14.375rem; height: 4rem;">
+
+                        </div>
+                        <div style="width: 1px; height: 4rem; background: #B9B4B4;"></div>
+                        <div class="bg-danger" style="width: 8.25rem; height: 4rem;">
+
+                        </div>
+                        <div style="width: 1px; height: 4rem; background: #B9B4B4;"></div>
+                        <div class="bg-danger" style="width: 8.25rem; height: 4rem;">
+
+                        </div>
+                        <div
+                            style="width: 10.375rem; height: 5.625rem; background-color: #D9BBAC; border-radius: 1.25rem;">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section style="height: 30.5rem; margin-top: -7.6rem;">
+            <div style="padding-top: 4.5rem;">
+                <div class="d-flex justify-content-center align-items-center gap-5">
+                    <div>
+                        <h1 class="text" style="font-size: 4.063rem;">About Us</h1>
+                        <div style="width: 24.813rem; height: 15.75rem; font-size: 0.906rem;">
+                            <p>A service-based corporation aiming to lessen the hassle of rental processes allowing the
+                                customer to rent through this platform. We offer various types of vehicles with an
+                                exceptional customer services making your travel a stress and wander-free.
+                            </p>
+                            <p>With our help, we assure you to have an exquisite travel-free; hopping in with the dream
+                                car that you aspire to ride, and an extraordinary experience to feel as you wander.
+                                Wherever your adventure takes you, the Hay Go Car Rental will provide the perfect ride
+                                and an exceptional support. Ready, HAY. GO!
+                            </p>
+                        </div>
+                    </div>
+                    <div class="bg-danger" style="width: 24.813rem; height: 21.75rem; border-radius: 0.938rem;">
+
+                    </div>
+                    <div class="d-flex flex-column align-items-center" style="gap: 1.2rem">
+                        <div class="d-flex flex-column align-items-center" style="width: 22.188rem; height: 8.188rem; background: #242B25; border-radius: 0.938rem;">
+                            <h1 class="text-white mt-4">6+ Years</h1>
+                            <p style="color: #B9B7B7; font-size: 1.25rem; margin-top: -0.9rem;">Experience</p>
+                        </div>
+                        <div class="bg-danger" style="width: 22.188rem; height: 12.438rem; border-radius: 0.938rem;"></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div style="height: 25.438rem; background: #242B25;">
+                <div>
+                    <div class="" style="margin-left: 6.375rem; padding-top: 2.625rem;">
+                        <h1 class="text-white" style="font-size: 4.063rem;">Our Services</h1>
+                    </div>
+                    <div class="d-flex justify-content-center align-items-center"
+                        style="margin-top: 2.5rem; gap: 4.063rem;">
+                        <div class="bg-light" style="width: 17.3rem; height: 11.3rem;"></div>
+                        <div class="bg-light" style="width: 17.3rem; height: 11.3rem;"></div>
+                        <div class="bg-light" style="width: 17.3rem; height: 11.3rem;"></div>
+                        <div class="bg-light" style="width: 17.3rem; height: 11.3rem;"></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section style="height: 76rem; background: #EBE7E4;">
+            <div>
+                <div class="text-center" style="padding-top: 6.563rem;">
+                    <h1 style="font-size: 5rem; color: #484848;">How It Works</h1>
+                </div>
+            </div>
+        </section>
